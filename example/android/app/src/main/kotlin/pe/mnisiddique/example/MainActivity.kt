c073623b57327a33cc8fe21a571e81913bc91c8a
mnisiddique <mnisiddique30@gmail.com>
2024-04-02 01:11:22 +0600

    chore: initiates mock url generator

---
+++ example/android/app/src/main/kotlin/pe/mnisiddique/example/MainActivity.kt
@@ -0,0 +1,5 @@
+package pe.mnisiddique.example
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ mock_url_generator/lib/mock_url_generator.dart
@@ -0,0 +1,7 @@
+library mock_url_generator;
+
+/// A Calculator.
+class Calculator {
+  /// Returns [value] plus 1.
+  int addOne(int value) => value + 1;
+}
